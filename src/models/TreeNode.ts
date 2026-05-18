c841149598da4e8d4b484daaefe6266228d08710
alvarengaflavio <banysan@msn.com>
2026-05-18 12:55:39 -0300

    Adicionar propriedade height à classe TreeNode

--- src/models/TreeNode.ts
+++ src/models/TreeNode.ts
@@ -2,10 +2,12 @@ export class TreeNode {
   value: number;
   left: TreeNode | null;
   right: TreeNode | null;
+  height: number;
 
   constructor(value: number) {
     this.value = value;
     this.left = null;
     this.right = null;
+    this.height = 1; // nó novo sempre começa com altura 1
   }
 }
